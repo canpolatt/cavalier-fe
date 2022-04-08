000df31171e62d9cef0b73643d4a8015a8bc2ef6
Armağan Dalkıran <armagandalkiran@Armagan-MacBook-Pro.local>
2022-04-08 09:46:33 +0300

    footer added

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -14,7 +14,7 @@ const Products = () => {
   }, []);
 
   return (
-    <>
+    <div className="flex-1">
       <Categories slidesPerView={5} />
       <div className="p-4 lg:flex lg:px-24 items-center justify-between">
         <input type="text" placeholder="search.." className="border p-2 mb-2 w-full lg:w-1/4" />
@@ -32,7 +32,7 @@ const Products = () => {
           <ProductCard key={idx} item={item} />
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
