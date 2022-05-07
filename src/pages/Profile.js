80bd28061d1a86c7f90d7a8cee909de6abd8ed6a
Armagan Dalkiran <armagandalkran20@gmail.com>
2022-05-07 16:45:03 +0300

    navbar bug fixed

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,15 @@
+import useAuth from "../hooks/useAuth";
+
 const Profile = () => {
+  const user = useAuth();
   const isLoggedIn = localStorage.getItem("accessToken");
 
   return (
     <div className="flex-1">
       {isLoggedIn ? (
-        <div>Profile</div>
+        <div>
+          <h2>Merhaba {user.name},</h2>
+        </div>
       ) : (
         <div className="w-full h-full flex items-center justify-center text-3xl">
           Lütfen giriş yapınız.
